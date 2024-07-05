22f5521dd84270b41b296b98a7299e3932e74940
ThomEckert <thec.lernt@gmx.de>
2024-07-05 12:31:10 +0200

    ngmodel - variable with imput

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -9,7 +9,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -23,9 +25,18 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatDialogActions,
     MatDialogTitle,
     MatDatepickerModule,
-    MatNativeDateModule,
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {}
+export class DialogAddUserComponent {
+  user: User = new User();
+  birthDate!: Date;
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('Current user is', this.user);
+    
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,18 +4,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
-
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
 
+  user = new User();
+  
   constructor(public dialog: MatDialog) {}
+
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
   }
