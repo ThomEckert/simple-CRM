04b6b5008e0317fe39dfb8d8ee71fef43fc113af
ThomEckert <thec.lernt@gmx.de>
2024-09-03 15:29:06 +0200

    add dialog component

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [ MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatDialogModule ],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [ MatIconModule, MatButtonModule, MatTooltipModule,  ],
+  imports: [ MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openAddUserDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 
 }
