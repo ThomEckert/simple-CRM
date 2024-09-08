6b057846d3ed943bf8d8436e29719eed0865c0ab
ThomEckert <thec.lernt@gmx.de>
2024-09-08 17:12:23 +0200

    dialog-add-user: add imputfield email user.class: add email

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -3,6 +3,7 @@ export class User {
     userId: string;
     firstName: string;
     lastName: string;
+    eMail: string;
     birthDate: number;
     street: string;
     zipCode: number;
@@ -12,6 +13,7 @@ export class User {
         this.userId = obj ? obj.userId : '';
         this.firstName = obj ? obj.firstName : '';
         this.lastName = obj ? obj.lastName : '';
+        this.eMail = obj ? obj.eMail : '';
         this.birthDate = obj ? obj.birthDate : '';
         this.street = obj ? obj.street : '';
         this.zipCode = obj ? obj.zipCode : '';
@@ -23,6 +25,7 @@ export class User {
             userId: this.userId,
             firstName: this.firstName,
             lastName: this.lastName,
+            eMail: this.eMail,
             birthDate: this.birthDate,
             street: this.street,
             zipCode: this.zipCode,
