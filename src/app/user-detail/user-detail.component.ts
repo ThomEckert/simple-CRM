9ec79034212185917013603ebc5c6ca8744acedd
ThomEckert <thec.lernt@gmx.de>
2024-09-23 12:19:23 +0200

    edit button at user-detail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -4,20 +4,33 @@ import { ActivatedRoute } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [ MatCardModule, MatProgressSpinnerModule, CommonModule ],
+  imports: [
+    MatCardModule,
+    MatProgressSpinnerModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
-export class UserDetailComponent implements OnInit{
+export class UserDetailComponent implements OnInit {
   userID = '';
   userDetail: any = [];
   isLoading: boolean = false;
 
-  constructor(private route:ActivatedRoute, public firebaseService: FirebaseService) {}
+  constructor(
+    private route: ActivatedRoute,
+    public firebaseService: FirebaseService
+  ) {}
 
   async ngOnInit(): Promise<void> {
     this.userID = this.route.snapshot.paramMap.get('id') || '';
@@ -30,11 +43,10 @@ export class UserDetailComponent implements OnInit{
       console.error('No user ID provided');
       return;
     }
-
     this.isLoading = true;
     try {
       this.userDetail = await this.firebaseService.getUserById(this.userID);
-      console.log('User:', this.userDetail);      
+      console.log('User:', this.userDetail);
     } catch (error) {
       console.error('Error loading user data:', error);
     } finally {
@@ -42,4 +54,7 @@ export class UserDetailComponent implements OnInit{
     }
   }
 
+  openAddressDialog() {
+    console.log('Open address dialog');
+  }
 }
